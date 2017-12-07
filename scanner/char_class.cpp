37aba0d3e73cea8d0d1149c045ccbdc6ea412aaf
Dmitry <dmitr5.11.1997@gmail.com>
2017-12-07 15:03:04 +0200

    Added char_class implementation

--- scanner/char_class.cpp
+++ scanner/char_class.cpp
@@ -1,12 +1,17 @@
 #include "char_class.h"
 
+char_class::char_class(std::string const& name, std::regex const& rexp)
+    : name_(name), rexp_(rexp) {}
 
+char_class::char_class(std::string&& name, std::regex&& rexp)
+    : name_(std::move(name)), rexp_(std::move(rexp)) {}
 
-char_class::char_class()
+std::string const & char_class::get_name() const
 {
+    return name_;
 }
 
-
-char_class::~char_class()
+bool char_class::belongs(char c) const
 {
+    return std::regex_match(c, rexp_);
 }
--- scanner/char_class.h
+++ scanner/char_class.h
@@ -1,10 +1,20 @@
 #pragma once
+#include <regex>
+#include <string>
+
 class char_class
 {
 public:
-	char_class();
-	~char_class();
+    char_class() = default;
+    char_class(std::string const& name, std::regex const& rexp);
+    char_class(std::string&& name, std::regex&& rexp);
+    ~char_class() = default;
 
+    std::string const& get_name() const;
+    bool belongs(char c) const;
 
+private:
+    std::string name_;
+    std::regex  rexp_;
 };
 
